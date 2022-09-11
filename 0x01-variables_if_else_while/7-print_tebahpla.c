7555736f9ec1155d4ccdc8e1d0d1800703aaa22c
Thankgod Job <thankgodzzy1@gmail.com>
2022-09-11 03:05:42 -0700

    Added task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse
- * 
- * Return: 0
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
-int main(void)
 {
-	char letter;
+	char ch;
 
-	for (letter = 'z'; letter >= 'a'; letter--)
-		putchar(letter);
-
+	for (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
 	putchar('\n');
-
 	return (0);
 }
